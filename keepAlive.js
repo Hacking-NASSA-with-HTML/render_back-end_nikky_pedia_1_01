1fb82f252cce631989e9bb626ea0447ab66c787b
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-25 00:17:55 +0400

    fix: keep alive path fix

--- keepAlive.js
+++ keepAlive.js
@@ -4,7 +4,7 @@ export const keepAlive = (req, res) => {
     if (interval) return res.end()
 
     interval = setInterval(async () => {
-        await fetch("https://render-back-end-nikky-pedia.onrender.com")
+        await fetch("https://render-back-end-nikky-pedia.onrender.com/keep-alive")
     }, 60000)
 
     return res.end()
